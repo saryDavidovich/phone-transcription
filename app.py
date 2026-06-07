a1e0e6e9939e93e4b796d95c1cb16a43d64aad59
sary <131244496+saryDavidovich@users.noreply.github.com>
2026-06-07 12:30:07 +0300

    Set maximum content length to 50 MB

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_app():
         database_url = database_url.replace('postgres://', 'postgresql://', 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_pre_ping': True,
     'pool_recycle': 300,
